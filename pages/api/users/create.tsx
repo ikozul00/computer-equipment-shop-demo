09d19b5bb4bb698381fb4907ebd80a1a399d4df5
ikozul00 <ivana.kozul5@gmail.com>
2025-10-26 19:57:48 +0100

    Initialize orders for a user when created

--- pages/api/users/create.tsx
+++ pages/api/users/create.tsx
@@ -5,6 +5,73 @@ type ResponseData = {
     userId: number | null;
 }
 
+const createOrders = (userId) => ([
+    {
+        data: {
+            user: { connect: { id: userId } },
+            status: "pending",
+            orderList: {
+                create: [
+                    { quantity: 3, product: { connect: { id: 2 } } },
+                    { quantity: 2, product: { connect: { id: 3 } } },
+                ],
+            },
+        },
+        include: {
+            orderList: { include: { product: true } },
+            user: true,
+        },
+    },
+    {
+        data: {
+            user: { connect: { id: userId } },
+            status: "success",
+            orderList: {
+                create: [
+                    { quantity: 1, product: { connect: { id: 1 } } },
+                ],
+            },
+        },
+        include: {
+            orderList: { include: { product: true } },
+            user: true,
+        },
+    },
+    {
+        data: {
+            user: { connect: { id: userId } },
+            status: "failed",
+            orderList: {
+                create: [
+                    { quantity: 1, product: { connect: { id: 4 } } },
+                    { quantity: 2, product: { connect: { id: 2 } } },
+                ],
+            },
+        },
+        include: {
+            orderList: { include: { product: true } },
+            user: true,
+        },
+    },
+    {
+        data: {
+            user: { connect: { id: userId } },
+            status: "pending",
+            orderList: {
+                create: [
+                    { quantity: 1, product: { connect: { id: 4 } } },
+                    { quantity: 2, product: { connect: { id: 2 } } },
+                    { quantity: 4, product: { connect: { id: 3 } } },
+                ],
+            },
+        },
+        include: {
+            orderList: { include: { product: true } },
+            user: true,
+        },
+    }
+]);
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
     if (req.method !== "POST") {
         return res.status(405);
@@ -22,6 +89,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
                 email: email.toLowerCase()
             }
         })
+        console.log(user);
+        if (user) {
+            createOrders(user.id).forEach(async (order) => {
+                await prisma.order.create(order);
+            });
+        }
+
         res.status(201).json({ userId: user.id });
     } catch (error) {
         res.status(500).json({ userId: null });
